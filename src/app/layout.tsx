4d6f27800586e7869e626859d88d0ffb5e2ffa5e
Martín Stanicio <martuytimo@gmail.com>
2024-04-03 21:09:34 -0300

    add sticky footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Inter } from "next/font/google";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import { envVariables } from "@/lib/env";
+import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 
 faConfig.autoAddCss = false;
@@ -23,7 +24,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es" suppressHydrationWarning>
-      <body className={font.className}>
+      <body
+        className={cn(
+          font.className,
+          "grid min-h-screen grid-rows-[auto_auto_1fr_auto]",
+        )}
+      >
         <Header />
         {children}
         <Footer />
